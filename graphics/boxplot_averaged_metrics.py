7a39b17cb5ff132ccd842643bce0e6964659cae3
antoine-dedieu <tonio.dedieu@gmail.com>
2017-06-15 00:26:50 -0400

    6.1 - Fix bugs

--- graphics/boxplot_averaged_metrics.py
+++ graphics/boxplot_averaged_metrics.py
@@ -11,15 +11,20 @@ def boxplot_averaged_metrics(metric_to_plot, name_metric):
 
     fig = plt.figure(figsize=(10,5))
     ax  = fig.add_subplot(1,1,1)
-    bp  = ax.boxplot(metric_to_plot, 0, '')
-
 
     colors ={0:'g', 1:'b', 2:'#FFA500', 3:'#FFA500'}
 
     dict_accuracy_sparsity_length = {'l2_estimation':4, 'misclassification':4, 'sparsity':3, 'true_positive':3}
+    number_plots = dict_accuracy_sparsity_length[name_metric]
+
+    #new_metric_to_plot = []
+    #for j in range(len(metric_to_plot)):
+    #    new_metric_to_plot.append( metric_to_plot[j][:number_plots].tolist() )
 
 #---Boxplots
-    for i in range(dict_accuracy_sparsity_length[name_metric]):
+    bp  = ax.boxplot(metric_to_plot, 0, '')
+
+    for i in range(number_plots):
         box = bp['boxes'][i]
 
     #---Color inside of the box
@@ -44,20 +49,19 @@ def boxplot_averaged_metrics(metric_to_plot, name_metric):
 
 
 #---Labels
-    ax.set_xticks(range(dict_accuracy_sparsity_length[name_metric]))
+    ax.set_xticks(range(1, number_plots+1))
 
     dict_accuracy_sparsity_labels = {'l2_estimation'    :('L1+L0', 'L2+L0', 'L1', 'L2'), 
                                      'misclassification':('L1+L0', 'L2+L0', 'L1', 'L2'),
                                      'sparsity'         :('L1+L0', 'L2+L0', 'L1'),
                                      'true_positive'    :('L1+L0', 'L2+L0', 'L1')}
     ax.set_xticklabels(dict_accuracy_sparsity_labels[name_metric])
+    ax.set_xlim(right = number_plots+0.5)
 
 
     for ticks in [ax.xaxis.get_major_ticks(), ax.yaxis.get_major_ticks()]:
         for tick in ticks:
             tick.label.set_fontsize(14) 
-
-    ax.set_ylim(bottom=0)
 
     ax.set_title(name_metric, fontsize=18,loc='center')
 
--- synthetic_datasets/main_classification_synthetic_datasets.py
+++ synthetic_datasets/main_classification_synthetic_datasets.py
@@ -6,12 +6,12 @@ type_Sigma = 4
 
 rho_dmu_Sigma2  = [(.2,.5), (.2,1), (.2,3), (.2,5), 
 				   (.5,.5), (.5,1), (.5,3), (.5,5)]
-rho_dmu_Sigma4  = [(.2,1), (.2,2), (.2,5), (.2,10), 
-				   (.5,1), (.5,2), (.5,5), (.5,10)]
+rho_dmu_Sigma4  = [(.2,1), (.2,2), (.2,5), (.2,100), 
+				   (.5,1), (.5,2), (.5,5), (.5,100)]
 dict_rho_dmu = {2: rho_dmu_Sigma2, 4: rho_dmu_Sigma4}
 
 rho, d_mu = dict_rho_dmu[type_Sigma][int(sys.argv[1])]
 
 
-n_average = 1
-average_simulations_compare_methods_classification('hinge', 50, 100, 7, rho, d_mu, type_Sigma, n_average)
+n_average = 10
+average_simulations_compare_methods_classification('logreg', 50, 100, 7, rho, d_mu, type_Sigma, n_average)
